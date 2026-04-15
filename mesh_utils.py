dd7af4b34344e1e71bc0ee5f49f64959d4a42512
nikleet <nikleet@outlook.com>
2026-04-15 21:10:31 +0200

    Separated mesh utils into mesh_utils.py

---
+++ mesh_utils.py
@@ -0,0 +1,74 @@
+import pyvista as pv
+import numpy as np
+
+from pyvisual.core._mesh_parser import (
+    parse_mesh_params, parse_stack_params, 
+    mesh_grid_scales, stack_grid_scales, unravel_values
+)
+
+# TODO: Double check that this works and implement in scenen_manager
+
+def create_mgram_mesh(*args, 
+                      iframe='rtp', 
+                      oframe='xyz', 
+                      forder=True, 
+                      v_name="Data"):
+    """Explicit builder for 2D slices (Magnetograms) as StructuredGrids."""
+    # Parse arguments
+    parsed = parse_mesh_params(*args)
+    if len(parsed) not in (3, 4):
+        raise ValueError(f"Expected 3 or 4 arguments, got {len(parsed)}")
+
+    # Build 3D meshgrid and transform coordinates
+    scales = mesh_grid_scales(*parsed[:3], iframe=iframe, oframe=oframe, forder=forder)
+
+    # Strict Validation
+    if scales[0].shape.count(1) != 1:
+        raise ValueError(f"2D slices should have one fixed dimension, got {scales[0].shape}")
+
+    # Build Geometry
+    grid = pv.StructuredGrid(*scales)
+
+    # Attach unraveled point data if provided
+    if len(parsed) == 4:
+        grid[v_name] = unravel_values(parsed[3], scales[0].shape, forder=forder)
+
+    return grid
+
+
+def create_fieldline_mesh(*args, 
+                          iframe='rtp', 
+                          oframe='xyz', 
+                          forder=True, 
+                          v_name="Data"):
+    """Explicit builder for multi-splines (Fieldlines) as PolyData."""
+    # Parse arguments
+    parsed = parse_stack_params(*args)
+    if len(parsed) not in (3, 4):
+        raise ValueError(f"Expected 3 or 4 arguments, got {len(parsed)}")
+
+    # Transform coordinates (maintains stack shape)
+    scales = stack_grid_scales(*parsed[:3], iframe=iframe, oframe=oframe, forder=forder)
+
+    # Strict Validation
+    if scales[0].ndim != 2:
+        raise ValueError(f"Multi-spline arrays should be 2D, got {scales[0].ndim}D")
+
+    # Geometry Math
+    splines = np.stack(scales, axis=1).transpose((2, 0, 1)).reshape(-1, 3)
+    spline_len, num_splines = scales[0].shape
+    cells = np.column_stack(
+        (np.repeat(spline_len, num_splines), 
+         np.arange(spline_len * num_splines).reshape(num_splines, spline_len))
+    ).ravel()
+
+    # Build Geometry
+    grid = pv.PolyData()
+    grid.points = splines
+    grid.lines = cells
+
+    # Attach unraveled point data if provided
+    if len(parsed) == 4:
+        grid[v_name] = unravel_values(parsed[3], scales[0].shape, forder=forder)
+
+    return grid
---
+++ utils.py
@@ -0,0 +1,73 @@
+# utils.py
+
+import numpy as np
+import datetime as dt
+from glob import glob
+import pyvista as pv
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from pyvisual.core._typing import FrameType, FlColorType
+
+class MplColorHelper:
+    """
+        Helper class to get evently spaced color samples from continuous 
+        matplotlib colormaps. 
+    """
+    def __init__(self, cmap_name, start_val, stop_val):
+        self.cmap_name = cmap_name
+        self.cmap = plt.get_cmap(cmap_name)
+        self.norm = mpl.colors.Normalize(vmin=start_val, vmax=stop_val)
+        self.scalarMap = cm.ScalarMappable(norm=self.norm, cmap=self.cmap)
+
+    def get_rgba(self, val):
+        r, g, b, a = self.scalarMap.to_rgba(val)
+        return int(r * 255), int(g * 255), int(b * 255), a
+
+
+def round_seconds(obj: dt.datetime) -> dt.datetime:
+    if obj.microsecond >= 500_000:
+        obj += dt.timedelta(seconds=1)
+    return obj.replace(microsecond=0)
+
+
+
+def mastime_to_ut(mas_times: str, t0: dt.datetime = None) -> list[dt.datetime]:
+    """
+    Converts mastimes dict to a list of UT datetimes.
+    
+    fname: dict   dict of sequence and mastimes from dumps or masTime .txt file  
+    t0: datetime    initial time at start of run
+    
+    Returns:
+    times_ut        A list of UT datetimes.
+    """
+
+    t_mas = np.asarray(list(mas_times.values()))
+    
+    # Convert MAS times to UT time
+    t_fac_mas = 1445.87 / (3600) 
+    t_hrs = t_mas*t_fac_mas
+
+    if not t0:
+        # If no initial time is given, will begin from Jan 1, 1990
+        t0 = dt.datetime(1990, 1, 1)
+        print("No initial time is given, will begin from Jan 1, 1990")
+    
+    times_ut = []
+    for j in range(0,len(t_hrs)):
+        t_shift = round_seconds(t0 + dt.timedelta(hours=t_hrs[j]))
+        times_ut.append(t_shift)
+
+    return times_ut
+    
+
+# ========= IO UTILITIES =========
+
+def get_mag_files(data_folder):
+    br_files = glob(data_folder + '/br*')
+    bt_files = glob(data_folder + '/bt*')
+    bp_files = glob(data_folder + '/bp*')
+    return list(zip(br_files, bt_files, bp_files))
+
